b7cdf5d2707746296670be3f9b155fb46f22235b
Hassan <hasnasir24@gmail.com>
2023-07-11 01:09:03 +0500

    bug: JWT not defined, status: resolved

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 /**
  * Middleware to authenticate JWT token.
  * @param {Object} req - The request object.
@@ -7,23 +8,23 @@
  */
 const authenticateToken = (req, res, next) => {
   // Get the token from the request headers or query parameters
-  const token = req.headers.authorization || req.query.token;
+  const token = req.headers.authorization || req.query.token
 
   if (!token) {
-    return res.status(401).json({ error: "No token provided" });
+    return res.status(401).json({ error: 'No token provided' })
   }
 
   // Verify the token
-  jwt.verify(token, "your_secret_key", (err, decoded) => {
+  jwt.verify(token, 'your_secret_key', (err, decoded) => {
     // replace your_secret_key in a production environment use environment variables
     if (err) {
-      return res.status(401).json({ error: "Invalid token" });
+      return res.status(401).json({ error: 'Invalid token' })
     }
 
     // Token is valid, attach the decoded data to the request
-    req.userId = decoded.userId;
-    next();
-  });
-};
+    req.userId = decoded.userId
+    next()
+  })
+}
 
-module.exports = { authenticateToken };
+module.exports = { authenticateToken }
